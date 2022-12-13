744b647a9aba5925b81d8447c75658d86b72a2da
rkntria <120444168+rkntria@users.noreply.github.com>
2022-12-13 22:46:29 +0800

    Delete AON directory

--- AON/js/common.js
+++
@@ -1,9 +0,0 @@
-class NavigationElement {
-
-  constructor(data) {
-    this.page = data.page;
-    this.title=data.title;
-    this.boxId=data.boxId;
-    this.secondaryBoxId=data.secondaryBoxId;
-  }
-}
